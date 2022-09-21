49198312154f31351fd1c494760b847b393fc855
huntken26 <huntken26@gmail.com>
2022-09-21 17:57:46 -0500

    added some javascript query selectors

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -3,3 +3,39 @@
 // repeat this for each question
 // set arrays and if then statements, functions etc.
 // plan it all out during my study group
+
+
+var countdown = document.querySelector("timer");
+var mainSection = document.querySelector("wrapper");
+var mainTitle = document.querySelector("title");
+var instructions = document.querySelector("startup-instructions");
+var button = document.querySelector("start-button");
+
+
+
+
+
+
+
+
+
+
+
+
+// var secondsLeft = 10;
+
+// function setTime() {
+//   // Sets interval in variable
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+//     if(secondsLeft === 0) {
+//       // Stops execution of action at set interval
+//       clearInterval(timerInterval);
+//       // Calls function to create and append image
+//       sendMessage();
+//     }
+
+//   }, 1000);
+// }

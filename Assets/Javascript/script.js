21eea0c3bbc1870e1e731c182ccd06f58947ff41
huntken26 <huntken26@gmail.com>
2022-09-21 07:48:28 -0500

    finished initial css, added some javascript comments

---
+++ Assets/Javascript/script.js
@@ -0,0 +1,5 @@
+//Need to set variables/  get element by id's for the h1, text, and button.
+// Those will all dissapear upon on click event, then the first question will pop up. append this and set styles within JS
+// repeat this for each question
+// set arrays and if then statements, functions etc.
+// plan it all out during my study group

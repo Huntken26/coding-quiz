93897c3d0a94f52d2f4892e4861bbeb02fe79a10
huntken26 <huntken26@gmail.com>
2022-09-26 01:39:44 -0500

    Finished Questions

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,9 +1,7 @@
-//Need to set variables/  get element by id's for the h1, text, and button.
-// Those will all dissapear upon on click event, then the first question will pop up. append this and set styles within JS
-// repeat this for each question
-// set arrays and if then statements, functions etc.
-// plan it all out during my study group
-// click event for timer/button and another event for wrong answerr
+// Set up my styling for the questions, answers, etc. Set up the timer, if then statements for each question and choice
+// set up if then statements for my choices being able to penalize someone for choosing wrong answer
+//set up final page created elements and also styling. High score page and elements
+// if anything other than correct answer is chosen alert wrong, is chosen right alert correct
 
 var body = document.body;
 var countdown = document.querySelector(".timer");
@@ -13,138 +11,97 @@ var instructions = document.querySelector(".startup-instructions");
 var start = document.querySelector(".start-button");
 var highScores = document.querySelector(".top-left");
 
-
 var question = document.createElement("div");
-var listEl  = document.createElement("ol");
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
+var listEl = document.createElement("ol");
+var li1 = document.createElement("BUTTON");
+var li2 = document.createElement("BUTTON");
+var li3 = document.createElement("BUTTON");
+var li4 = document.createElement("BUTTON");
 
-question.textContent = "";
-li1.textContent = "";
-li2.textContent = "";
-li3.textContent = " ";
-li4.textContent = "";
+question.setAttribute("class", "questions");
 
-
-mainSection.appendChild(question);
-question.appendChild(listEl);
-listEl.appendChild(li1);
-listEl.appendChild(li2);
-listEl.appendChild(li3);
-listEl.appendChild(li4);
-
-
-start.onclick = function() {
-    mainTitle.style.visibility = "hidden";
-    instructions.style.visibility = "hidden";
-    start.style.visibility = "hidden";
-};
-
-
-function questionOne(questionsArray) {
-
+start.onclick = function () {
+  mainTitle.style.visibility = "hidden";
+  instructions.style.visibility = "hidden";
+  start.style.visibility = "hidden";
+  body.appendChild(question);
+  question.appendChild(listEl);
+  listEl.appendChild(li1);
+  listEl.appendChild(li2);
+  listEl.appendChild(li3);
+  listEl.appendChild(li4);
+  return questionOne();
 }
 
-function questionTwo(questionsArray) {
-    
+function questionOne() {
+  question.textContent = "Which of these data types is a string?";
+  question.appendChild(listEl);
+  listEl.appendChild(li1);
+  listEl.appendChild(li2);
+  listEl.appendChild(li3);
+  listEl.appendChild(li4);
+  li1.textContent = 7;
+  li2.textContent = " 'Seven' ";
+  li3.textContent = true;
+  li4.textContent = "undefined";
+li2.onclick = function () {
+    return questionTwo();
+}
 }
 
-function questionThree(questionsArray) {
-    
+function questionTwo() {
+  question.textContent = "Which of these is used for Front-End Development?";
+  question.appendChild(listEl);
+  listEl.appendChild(li1);
+  listEl.appendChild(li2);
+  listEl.appendChild(li3);
+  listEl.appendChild(li4);
+  li1.textContent = "Ruby";
+  li2.textContent = "Java";
+  li3.textContent = "Javascript";
+  li4.textContent = "Python";
+  li3.onclick = function () {
+    return questionThree();
+}
 }
 
-function questionFour(questionsArray) {
-    
+function questionThree() {
+  question.textContent = "What does HTML stand for ?";
+  question.appendChild(listEl);
+  listEl.appendChild(li1);
+  listEl.appendChild(li2);
+  listEl.appendChild(li3);
+  listEl.appendChild(li4);
+  li1.textContent = "Hyper Bolic Time Chamber";
+  li2.textContent = "High Transfer Meta Language";
+  li3.textContent = "Hexa Mega Texas Loop";
+  li4.textContent = "Hypertext Markup Language";
+  li4.onclick = function () {
+    return questionFour();
+}
 }
 
-function finalScore(questionsArray) {
 
+function questionFour() {
+  question.textContent = "_____ is used for Styling Websites ?";
+  question.appendChild(listEl);
+  listEl.appendChild(li1);
+  listEl.appendChild(li2);
+  listEl.appendChild(li3);
+  listEl.appendChild(li4);
+  li1.textContent = "C++";
+  li2.textContent = "SQL";
+  li3.textContent = "CSS";
+  li4.textContent = "CZZ";
+
+  li3.onclick = function () {
+    return finalScore();
+}
 }
 
+function finalScore() {
+    var score = document.createElement("div");
 
-var questionsArray = [
-    {
-        question: "Which of these data types is a string?",
-        optionA: 7,
-        optionB: "Seven",
-        optionC: true,
-        optionD: undefined,
-        correct: "optionB"
-    },
+    score = "";
+}
 
-    {
-        question: "Which of these is used for Front-End Development?",
-        optionA: "Ruby",
-        optionB: "Java",
-        optionC: "Javascript",
-        optionD: "Python",
-        correct: "optionC"
-    },
-
-    {
-        question: "What does HTML stand for ?",
-        optionA: "Hyper Bolic Time Chamber",
-        optionB: "High Transfer Meta Language",
-        optionC: "Hexa Mega Texas Loop",
-        optionD: "Hypertext Markup Language",
-        correct: "optionD"
-    },
-
-    {
-        question: "_____ is used for Styling Websites ?",
-        optionA: "C++",
-        optionB: "SQL",
-        optionC: "CSS",
-        optionD: "CZZ",
-        correct: "optionC"
-    },
-
-]
-
-    
-
-highScores.addEventListener("click", function() {
-
-})
-
-
-
-// Listen for a click event on toggle switch
-// themeSwitcher.addEventListener("click", function() {
-//     // If mode is dark, apply light background
-//     if (mode === "dark") {
-//       mode = "light";
-//       container.setAttribute("class", "light");
-//     }
-
-// var secondsLeft = 75;
-
-// function setTime() {
-//   // Sets interval in variable
-//   var timerInterval = setInterval(function() {
-//     secondsLeft--;
-//     timeEl.textContent = " Timer:" + secondsLeft;
-
-//     if(secondsLeft === 0) {
-//       // Stops execution of action at set interval
-//       clearInterval(timerInterval);
-//       // Calls function to create and append image
-//       sendMessage();
-//     }
-
-//   }, 1000);
-// }
-
-
-// function sendMessage() {
-//     timeEl.textContent = " ";
-  
-//   }
-  
-
-
-//   setTime();
-  
-  
